7854549f7b20e3852ee5cf5bea59af21ae4c10be
harry-io <112643733+harry-io@users.noreply.github.com>
2023-04-08 19:33:27 +0530

    Auth updates

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,21 +2,23 @@ import Game from "./pages/Games";
 import Start from "./pages/Start";
 import Finished from "./pages/Finished";
 import useTickTackToe from "./hooks/useTicTacToe";
-
+import AllRoutes from "./Routes/AllRoutes";
 
 const App = () => {
   const game = useTickTackToe();
   return (
-    
-    <div className="flex justify-center">
-      {game.status === "created" && <Start handleStart={game.handleStart} />}
-      {game.status === "finished" && (
-        <Finished name={game.winner} restart={game.handleRestart} />
-      )}
-      {game.status === "started" && (
-        <Game board={game.board} handleClick={game.handleClick} />
-      )}
-    </div>
+    <>
+      <AllRoutes />
+      <div className="flex justify-center">
+        {game.status === "created" && <Start handleStart={game.handleStart} />}
+        {game.status === "finished" && (
+          <Finished name={game.winner} restart={game.handleRestart} />
+        )}
+        {game.status === "started" && (
+          <Game board={game.board} handleClick={game.handleClick} />
+        )}
+      </div>
+    </>
   );
 };
 export default App;
